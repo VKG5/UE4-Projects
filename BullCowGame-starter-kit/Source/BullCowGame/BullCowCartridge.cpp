08de1b57ae5ab09b7303799f7ad2244b94127dec
Lumi_V <bawagupta69@gmail.com>
2021-03-10 18:47:04 +0530

    Made a basic working game along with a few options like restarting, etc.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,42 +5,101 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     
-    // Welcome the Player
-    // Print a line in the terminal
-    PrintLine(TEXT("Welcome to Bull Cows!\nPress Enter to continue..."));
+    SetupGame();
 
-    // Declaring the hidden word
-    HiddenWord = TEXT("ambidextrously");
-
-    // TODO Set the Lives
-
-    // TODO Prompt the player for input
+    // Debug Line
+    // PrintLine(TEXT("The Hidden word is: %s"), *HiddenWord);
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
 
-    // TODO Evaluate the length of the Input string
-    
-    // TODO Update the lives/spaces left
-        // Check if lives > 0 show lives
-            // If yes, Guess Again
-            // If No, Show Word and Gameover
-
-            // TODO Count the number of Bulls and Cows
-
-    // Winning Condition
-    if (Input == HiddenWord)
+    if (bGameOver == true)
     {
-        PrintLine(Input);
+        // If game is over 
+        // clearscreen() and setupgame()
+        ClearScreen();
+        SetupGame();
     }
-       
-    // Lose condition
+
+    // Else check player guess
+    // TODO Count the number of Bulls and Cows
+
     else
     {
-        PrintLine(TEXT("You Lost!"));
+        // Process the Input
+        ProcessGame(Input);
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    // Initializations
+    // Declaring the hidden word
+    HiddenWord = TEXT("ambidextrously");
+
+    // Set the Lives
+    Lives = HiddenWord.Len();
+
+    // Set Game Over variable
+    bGameOver = false;
+
+    // Welcome the Player
+    // Print a line in the terminal
+    PrintLine(TEXT("Welcome to Bull Cows! \
+                    \nGuess the %i character word \
+                    \nYou have %i Lives \
+                    \nPress Enter to continue..."), HiddenWord.Len(), Lives); // Prompt the player for input
+}
+
+void UBullCowCartridge::EndGame() // When player hits Enter
+{
+    bGameOver = true;
+    PrintLine(TEXT("The word was : %s"), *HiddenWord);
+    PrintLine(TEXT("Press Enter to Play Again!"));
+}
+
+void UBullCowCartridge::ProcessGame(FString guess)
+{
+    // To get the length of the hidden word
+    int32 len = HiddenWord.Len();
+
+    // Length Condition
+    if (guess.Len() == len)
+    {
+        if (guess == HiddenWord)
+        {
+            // PrintLine(Input);
+            PrintLine("You have won!!!");
+            EndGame();
+        }
+
+        else
+        {
+            PrintLine(TEXT("Correct Length but Wrong Word!"));
+            LivesLeft();
+        }
+    }
+
+    // Length fail condition
+    else
+    {
+        PrintLine(TEXT("The Hidden Word is %i characters long!"), len);
+        LivesLeft();
     }
 
     // TODO Play Again or Quit
+}
+
+void UBullCowCartridge::LivesLeft()
+{
+    // Updating the lives
+    Lives--;
+    PrintLine(TEXT("You have %i lives remaining!"), Lives);
+
+    if (Lives == 0)
+    {
+        PrintLine(TEXT("**GAME OVER**"), Lives);
+        EndGame();
+    }
 }
--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -14,9 +14,19 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 		virtual void BeginPlay() override;
 		virtual void OnInput(const FString& Input) override;
+		void SetupGame();
+		void EndGame();
+		void ProcessGame(FString guess);
+		void LivesLeft();
 
 	// Your declarations go below!
 	private:
-	// Defining the HiddenWord
+		// Declaring the HiddenWord
 		FString HiddenWord;
+
+		// Declaring the Lives
+		int32 Lives;
+
+		// Declaring the Game Over 
+		bool bGameOver;
 };
